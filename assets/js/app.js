92de31cd8c92cd12a30058949a0a73e8390816a9
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2024-06-29 09:57:53 +0200

    done it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,20 @@
 const submitBtn = document.getElementById("submit");
-const modalBox = document.querySelector('.modal-box');
-const mainBox = document.querySelector('.main-box');
+const modalBox = document.querySelector(".modal-box");
+const mainBox = document.querySelector(".main-box");
+const counter = document.getElementById('counter');
 
-
+const ratingInputs = document.getElementsByName("rating");
+let rating = 0;
 submitBtn.addEventListener("click", (e) => {
-  console.log(modalBox);
-  modalBox.classList.add('active')
-  mainBox.classList.add('inactive')
+  let i = 0;
+  ratingInputs.forEach((input) => {
+    i++;
+    if (input.checked) {
+      rating = i
+
+    }
+  });
+  modalBox.classList.add("active");
+  counter.innerHTML = rating;
+  mainBox.classList.add("inactive");
 });
